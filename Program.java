f9f58b30354ce801f671b7f8fe8d2fa7e58f39c0
Jkatz5490 <Jkatz5490@gmail.com>
2022-05-27 12:43:49 -0400

    method add 3

--- Program.java
+++ Program.java
@@ -1,12 +1,20 @@
 import java.util.*;
 public class Program{
 	public static void main(String a[]){
-	
+		
 		System.out.println("Welcome to Jenkins");
+		print(41);
+		print2(10);
 
 	}
 
 	public static void print(int x){
+		for(int i=1; i<=10; i++){
+			System.out.print(x+"x"+i);
+		}
+	}
+	
+	public static void print2(int x){
 		for(int i=1; i<=10; i++){
 			System.out.print(x+"x"+i);
 		}
